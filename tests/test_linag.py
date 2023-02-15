9db38c15040323a06aa1e77215f126e1abaf27e1
owrior <o.w.riordan@gmail.com>
2023-02-15 11:46:33 +0000

    Alter testing to use sklearn dataset. Add benchmarking.

--- tests/test_linag.py
+++ tests/test_linag.py
@@ -1,23 +1,86 @@
 import pytest
 import rusty_learning as rl
 import numpy as np
+import timeit
+
+TIMEIT_NUMBER = 10
 
 
-def test_perceptron_predict(linearly_related_data):
+class Perceptron:
+    def __init__(self, learning_rate=0.01, n_iters=1000):
+        self.lr = learning_rate
+        self.n_iters = n_iters
+        self.activation_func = self._unit_step_func
+        self.weights = None
+        self.bias = None
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+
+        # init parameters
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        y_ = np.array([1 if i > 0 else 0 for i in y])
+
+        for _ in range(self.n_iters):
+            for idx, x_i in enumerate(X):
+                linear_output = np.dot(x_i, self.weights) + self.bias
+                y_predicted = self.activation_func(linear_output)
+
+                # Perceptron update rule
+                update = self.lr * (y_[idx] - y_predicted)
+
+                self.weights += update * x_i
+                self.bias += update
+
+    def predict(self, X):
+        linear_output = np.dot(X, self.weights) + self.bias
+        y_predicted = self.activation_func(linear_output)
+        return y_predicted
+
+    def _unit_step_func(self, x):
+        return np.where(x >= 0, 1, 0)
+
+
+def test_perceptron_predict(separable_data):
     weights = np.array([[10.0], [2], [3]])
-    x, y = linearly_related_data
-    res = rl.predict(weights, x)
-    print(res)
+    X, y = separable_data
+    res = rl.predict(weights, X)
 
 
-def test_perceptron_predict(linearly_related_data):
-    x = linearly_related_data[0]
-    y = linearly_related_data[1]
-    weights, accuracy = rl.train(x, y, 0.1, 10)
-    y_hat = rl.predict(
-        np.array([[-0.1], [0.20653640140000007], [-0.23418117710000003]]), x
+def test_perceptron_train(separable_data):
+    X, y = separable_data
+
+    weights, accuracy = rl.train(X, y.reshape((-1, 1)).astype(float), 0.01, 100)
+    y_hat = rl.predict(weights, X).flatten()
+
+    np.testing.assert_almost_equal(
+        weights, np.array([[3.53], [0.83434904], [1.59407104]])
     )
-    print(weights.flatten(), accuracy)
-    print(y.flatten())
-    print(y_hat.flatten())
-    print((y - y_hat).flatten())
+    assert 1 - ((y - y_hat.flatten()) ** 2).mean() == accuracy
+
+
+def test_perceptron_train_performance(separable_data):
+    X, y = separable_data
+
+    # Calculate rust implemted time
+    rl_time = np.round(
+        timeit.timeit(
+            lambda: rl.train(
+                X, y.reshape((-1, 1)).astype(float), alpha=0.01, n_epoch=1000
+            ),
+            number=TIMEIT_NUMBER,
+        ),
+        decimals=2,
+    )
+
+    # Calculate pure python numpy time
+    p = Perceptron(learning_rate=0.01, n_iters=1000)
+    python_time = np.round(
+        timeit.timeit(lambda: p.fit(X, y), number=TIMEIT_NUMBER), decimals=2
+    )
+
+    print(
+        f"Rust implementation: {rl_time}, Numpy implementation: {python_time}\nMultiple: {python_time / rl_time}"
+    )
