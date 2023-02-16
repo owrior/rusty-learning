2d0a208288c0b6be1f26daff547fb779d4193b10
Oscar Riordan <o.w.riordan@gmail.com>
2023-02-16 15:27:41 +0000

    Use the add_wrapped format of creating the module.

--- src/lib.rs
+++ src/lib.rs
@@ -4,33 +4,36 @@ use pyo3::{prelude::*, types::PyTuple};
 mod algo;
 use algo::*;
 
+#[pyfunction]
+fn train<'py>(
+    py: Python<'py>,
+    x: PyReadonlyArray2<f64>,
+    y: PyReadonlyArray2<f64>,
+    alpha: f64,
+    n_epoch: i64,
+) -> &'py PyTuple {
+    let x_array = x.as_array();
+    let y_array = y.as_array();
+
+    let res = perceptron::train(&x_array, &y_array, alpha, n_epoch);
+    PyTuple::new(py, vec![res.0.into_pyarray(py), res.1.into_pyarray(py)])
+}
+
+#[pyfunction]
+fn predict<'py>(
+    py: Python<'py>,
+    weights: PyReadonlyArray2<f64>,
+    x: PyReadonlyArray2<f64>,
+) -> &'py PyArray2<f64> {
+    let weights_array = weights.as_array();
+    let x_array = x.as_array();
+    let res = perceptron::predict(&weights_array, &x_array);
+    res.into_pyarray(py)
+}
+
 #[pymodule]
 fn rusty_learning(_py: Python, m: &PyModule) -> PyResult<()> {
-    #[pyfn(m)]
-    fn train<'py>(
-        py: Python<'py>,
-        x: PyReadonlyArray2<f64>,
-        y: PyReadonlyArray2<f64>,
-        alpha: f64,
-        n_epoch: i64,
-    ) -> &'py PyTuple {
-        let x_array = x.as_array();
-        let y_array = y.as_array();
-
-        let res = perceptron::train(&x_array, &y_array, alpha, n_epoch);
-        PyTuple::new(py, vec![res.0.into_pyarray(py), res.1.into_pyarray(py)])
-    }
-
-    #[pyfn(m)]
-    fn predict<'py>(
-        py: Python<'py>,
-        weights: PyReadonlyArray2<f64>,
-        x: PyReadonlyArray2<f64>,
-    ) -> &'py PyArray2<f64> {
-        let weights_array = weights.as_array();
-        let x_array = x.as_array();
-        let res = perceptron::predict(&weights_array, &x_array);
-        res.into_pyarray(py)
-    }
+    m.add_wrapped(wrap_pyfunction!(train)).unwrap();
+    m.add_wrapped(wrap_pyfunction!(predict)).unwrap();
     Ok(())
 }
