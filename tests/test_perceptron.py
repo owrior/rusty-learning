5d4eff1fbcd0d7be9caf8cc4454654be9f7b185a
Oscar Riordan <o.w.riordan@gmail.com>
2023-02-16 11:12:05 +0000

    Add makefile and python requirements.

--- tests/test_perceptron.py
+++ tests/test_perceptron.py
@@ -37,17 +37,17 @@ class BasicPerceptron:
 
     def predict(self, X):
         linear_output = np.dot(X, self.weights) + self.bias
-        y_predicted = self.activation_func(linear_output)
-        return y_predicted
+        return self.activation_func(linear_output)
 
     def _unit_step_func(self, x):
         return np.where(x >= 0, 1, 0)
 
 
 def test_perceptron_predict(separable_data):
-    weights = np.array([10.0, 2, 3])
+    weights = np.array([3.53, 1.59407104, 0.83434904])
     X, y = separable_data
     res = rl.predict(weights, X)
+    np.testing.assert_array_equal(res.flatten().astype(int), y)
 
 
 def test_perceptron_train(separable_data):
@@ -60,8 +60,34 @@ def test_perceptron_train(separable_data):
     assert 1 - ((y - y_hat.flatten()) ** 2).mean() == accuracy
 
 
+def test_benchmark_train_sklearn(separable_data):
+    X, y = separable_data
+
+    # Calculate rust implemted time
+    rl_time = np.round(
+        timeit.timeit(
+            lambda: rl.train(
+                X, y.reshape((-1, 1)).astype(float), alpha=0.01, n_epoch=1000
+            ),
+            number=TIMEIT_NUMBER,
+        ),
+        decimals=2,
+    )
+
+    # Calculate pure python numpy time
+    p = SKPerceptron(alpha=0.01, max_iter=1000)
+    comp_time = np.round(
+        timeit.timeit(lambda: p.fit(X, y), number=TIMEIT_NUMBER), decimals=2
+    )
+
+    print(
+        f"Rust implementation: {rl_time}, Sklearn implementation: {comp_time}\n"
+        f"Multiple: {comp_time / rl_time}, {'Faster' if rl_time < comp_time else 'Slower'}"
+    )
+
+
 @pytest.mark.skip
-def test_benchmark_perceptron_train(separable_data):
+def test_benchmark_train_numpy(separable_data):
     X, y = separable_data
 
     # Calculate rust implemted time
@@ -77,20 +103,11 @@ def test_benchmark_perceptron_train(separable_data):
 
     # Calculate pure python numpy time
     p = BasicPerceptron(learning_rate=0.01, n_iters=1000)
-    python_time = np.round(
+    comp_time = np.round(
         timeit.timeit(lambda: p.fit(X, y), number=TIMEIT_NUMBER), decimals=2
     )
 
     print(
-        f"Rust implementation: {rl_time}, Numpy implementation: {python_time}\nMultiple: {python_time / rl_time}\n"
-    )
-
-    # Calculate pure python numpy time
-    p = SKPerceptron(alpha=0.01, max_iter=1000)
-    python_time = np.round(
-        timeit.timeit(lambda: p.fit(X, y), number=TIMEIT_NUMBER), decimals=2
-    )
-
-    print(
-        f"Rust implementation: {rl_time}, Sklearn implementation: {python_time}\nMultiple: {python_time / rl_time}"
+        f"Rust implementation: {rl_time}, Sklearn implementation: {comp_time}\n"
+        f"Multiple: {comp_time / rl_time}, {'Faster' if rl_time < comp_time else 'Slower'}"
     )
