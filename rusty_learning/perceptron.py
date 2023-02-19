52d2a0450f5eee41966efa31fbb295bc8faa075b
Oscar Riordan <o.w.riordan@gmail.com>
2023-02-19 14:31:17 +0000

    Add back typing.

--- rusty_learning/perceptron.py
+++ rusty_learning/perceptron.py
@@ -1,5 +1,8 @@
 import rusty_learning.rusty_learning as _rl
+from numpy.typing import NDArray
 import numpy as np
+
+FloatArray = NDArray[np.float64]
 
 
 class Perceptron:
@@ -9,14 +12,14 @@ class Perceptron:
         self._p = _rl.Perceptron(alpha, n_epoch, num_features)
         self.weights = self._p.get_weights()
 
-    def train(self, x, y) -> float:
+    def train(self, x: FloatArray, y: FloatArray) -> float:
         accuracy = self._p.train(x, y)
         self.weights = self._p.get_weights()
         return accuracy
 
-    def predict(self, x):
+    def predict(self, x: FloatArray) -> FloatArray:
         return self._p.predict(x)
 
-    def set_weights(self, weights) -> None:
+    def set_weights(self, weights: FloatArray) -> None:
         self._p.set_weights(weights)
         self.weights = self._p.get_weights()
